2b790d1b88f1cd3bde5b4571f31c3772b6e29ac0
Marcin <Marcin@DESKTOP-8GRD55Q>
2016-03-19 18:47:38 +0100

    start

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
            // new AppBundle\AppBundle(),
             new Marcin\AdminBundle\MarcinAdminBundle(),
+            new Common\UserBundle\CommonUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
